6daff8a7b15135608a57eb0f3f0b9ca1998a1683
angelgimenez85 <gimenezangel84@gmail.com>
2022-05-08 21:47:02 -0300

    Video 37

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import SongSearch from './components/SongSearch';
 import CrudApp from './components/CrudApp';
 import CrudApi from './components/CrudApi';
 
@@ -7,6 +8,9 @@ export default function App() {
   return (
     <div>
       <h1>Ejercicios con React</h1>
+      <hr />
+      <SongSearch />
+      <hr />
       <CrudApi />
       <hr />
       <CrudApp />
---
+++ src/components/SongForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SongForm = ({ handeSearch }) => {
+  return (
+    <div>
+      <h2>Formulario:</h2>
+    </div>
+  );
+};
+
+export default SongForm;
