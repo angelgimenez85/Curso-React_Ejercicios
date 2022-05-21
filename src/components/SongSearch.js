76ce7cd203fb260f2cd45e34cba88bfcbd0deadf
angelgimenez85 <gimenezangel84@gmail.com>
2022-05-20 22:12:14 -0300

    Creacion del formulario

--- src/components/SongSearch.js
+++ src/components/SongSearch.js
@@ -9,15 +9,16 @@ const SongSearch = () => {
   const [bio, setBio] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const handeSearch = (data) => {
+  const handleSearch = (data) => {
     console.log(data);
+    setSearch(data);
   };
 
   return (
     <div>
       <h2>Song Search</h2>
       {loading && <Loader />}
-      <SongForm handeSearch={handeSearch} />
+      <SongForm handleSearch={handleSearch} />
       <SongDetail search={search} lyric={lyric} bio={bio} />
     </div>
   );
